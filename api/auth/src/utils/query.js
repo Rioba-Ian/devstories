d5a3e7960ef9ad97bd9a8960a42c06f61096620d
alfygitau <alfygitau@gmail.com>
2023-06-19 14:09:37 +0300

    fixed github issue

--- api/auth/src/utils/query.js
+++ api/auth/src/utils/query.js
@@ -1,7 +1,6 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
-  connectionString:
-    "postgres://devstory_group:kZLLtAx1kW9DqcDggZTS32gqeYhwc2kW@dpg-ci81citiuie0h35ciu4g-a.oregon-postgres.render.com/devstories?ssl=true",
+  connectionString:process.env.CONNECTION_STRING
 });
 
 module.exports = pool;
